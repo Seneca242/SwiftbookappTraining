2bff25f78005964187c56df79bc9db2cb1f57465
Seneca242 <flabbergasted242@gmail.com>
2023-03-08 21:51:44 +0200

    finished as MVC

--- SwiftbookappTraining/CourseDetailsViewController.swift
+++ SwiftbookappTraining/CourseDetailsViewController.swift
@@ -11,9 +11,27 @@ class CourseDetailsViewController: UIViewController {
     
     private lazy var courseNameLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "Arial", size: 14)
+        label.font = UIFont(name: "Menlo Regular", size: 23)
         label.text = course.name
+        label.textAlignment = .center
         return label
+    }()
+    
+    private lazy var favouriteButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.addTarget(self, action: #selector(toggleFavourite), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.distribution = .fill
+        stackView.spacing = 20
+        return stackView
     }()
     
     private lazy var numberOfLessonsLabel: UILabel = {
@@ -35,12 +53,6 @@ class CourseDetailsViewController: UIViewController {
         return image
     }()
     
-    private lazy var favouriteButton: UIButton = {
-        let button = UIButton()
-        button.addTarget(self, action: #selector(toggleFavourite), for: .touchUpInside)
-        return button
-    }()
-    
     var course: Course!
     
     private var isFavourite = false
@@ -48,14 +60,22 @@ class CourseDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        addSubviews(to: courseNameLabel, numberOfLessonsLabel, numberOfTestsLabel, courseImage, favouriteButton)
+        addSubviews(to: courseNameLabel, courseImage, favouriteButton, stackView)
+        addSubviewsToStackView(subviews: numberOfLessonsLabel, numberOfTestsLabel)
         loadFavouriteStatus()
         setupUI()
+        setupConstraints()
     }
 
     private func addSubviews(to views: UIView...) {
         views.forEach { subView in
             view.addSubview(subView)
+        }
+    }
+    
+    private func addSubviewsToStackView(subviews: UIView...) {
+        subviews.forEach { subview in
+            stackView.addArrangedSubview(subview)
         }
     }
     
@@ -87,6 +107,51 @@ class CourseDetailsViewController: UIViewController {
     
     private func loadFavouriteStatus() {
         isFavourite = DataManager.shared.getFavoriteStatus(for: course.name)
+    }
+    
+    private func setupConstraints() {
+        courseNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            courseNameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 39),
+            courseNameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            courseNameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+        ])
+        
+        courseImage.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            courseImage.topAnchor.constraint(equalTo: courseNameLabel.bottomAnchor, constant: 27),
+            courseImage.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 60),
+            courseImage.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -60),
+            courseImage.heightAnchor.constraint(equalToConstant: 203)
+        ])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: courseImage.bottomAnchor, constant: 48),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 34),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -34)
+        ])
+        
+        favouriteButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            favouriteButton.trailingAnchor.constraint(equalTo: courseImage.trailingAnchor, constant: 10),
+            favouriteButton.bottomAnchor.constraint(equalTo: courseImage.bottomAnchor, constant: -10),
+            favouriteButton.heightAnchor.constraint(equalToConstant: 40),
+            favouriteButton.widthAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        favouriteButton.imageView?.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            favouriteButton.imageView?.leadingAnchor.constraint(equalTo: favouriteButton.leadingAnchor),
+            favouriteButton.imageView?.trailingAnchor.constraint(equalTo: favouriteButton.trailingAnchor),
+            favouriteButton.imageView?.topAnchor.constraint(equalTo: favouriteButton.topAnchor),
+            favouriteButton.imageView?.bottomAnchor.constraint(equalTo: favouriteButton.bottomAnchor)
+        ].compactMap { $0 })
     }
     
 }
